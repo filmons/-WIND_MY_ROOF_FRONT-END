e373a572a40552f81fc4b54ceaa13a25064bce16
filmons <filmonseare@gmail.com>
2021-12-18 15:21:34 +0100

    updated the route to react router dom and End create the heather ⚡️⚡️⚡️

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch,Redirect, } from "react-router-dom";
+import Header from "./components/Header";
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
+    <div>
+			<Header/>
+			<Router>
+				<Switch>
+				
+				</Switch>
+			</Router>
+		</div>
   );
 }
 
